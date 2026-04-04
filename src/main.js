3259c6c0cc254ee2dc7de50d189d2455e18c55f2
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-04-04 09:33:28 -0600

    add order modal

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 import './css/modern-normalize.css';
 import './css/styles.css';
+
+
+
+
+
+
+// ORDER MODAL ↓↓↓
+
+const orderModal = document.getElementById("orderModal");
+const openOrderBtn = document.getElementById("openOrderModal"); /// первірити назву кнопки
+const closeBtn = orderModal.querySelector(".order-close");
+
+openOrderBtn.onclick = () => orderModal.style.display = "block";
+closeBtn.onclick = () => orderModal.style.display = "none";
+window.onclick = (e) => { if (e.target == orderModal) orderModal.style.display = "none"; }
+
+document.getElementById("orderForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Заявка відправлена!");
+  orderModal.style.display = "none";
+  this.reset();
+});
+
+//ORDER MODAL ↑↑↑
