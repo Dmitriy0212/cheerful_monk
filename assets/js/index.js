837c566add83f684b8907f890947a47d75ae767f
Dmitriy0212 <57304379+Dmitriy0212@users.noreply.github.com>
2026-04-05 13:08:36 +0000

    Deploying to gh-pages from @ Dmitriy0212/cheerful_monk@d3701e68043b3fc5a48cc5fbf9e217881414bff5 🚀

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@
     <li 
       class="categories-item ${e.withAccent?"accent":""}"
       data-id="${e._id}"
-      style="background-image: url('/img/furniture-list-section/${e.image}');"
+      style="background-image: url('../img/furniture-list-section/${e.image}');"
     >
       <h4 class="categories-item__title">${e.name}</h4>
     </li>
