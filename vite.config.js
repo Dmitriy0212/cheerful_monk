04c992330e4580496ccd9e3a3614f867a436a555
Dmitriy0212 <demarr68@gmail.com>
2026-04-05 21:03:12 +0300

    some fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({}) => ({
+export default defineConfig(({ command }) => ({
   root: 'src',
 
   resolve: {
