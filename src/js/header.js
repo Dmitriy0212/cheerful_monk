f610da10384443f8dbe8ef54407a9ba83dfaeb32
Dmitriy0212 <demarr68@gmail.com>
2026-04-09 13:15:21 +0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -81,11 +81,6 @@ document.querySelectorAll('a[href*="#"]').forEach(anchor => {
   });
 });
 
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
-    toggleMenu();
-  }
-});
 burgerBtn.addEventListener('click', toggleMenu);
 closeBtn.addEventListener('click', toggleMenu);
 headerShadov.addEventListener('click', toggleMenuFromShadov);
